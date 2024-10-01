9f18a9d3a4edf783b767874c6179b694d1f8100a
Shivani Ambati <157499537+Shivaniambati006@users.noreply.github.com>
2024-10-01 23:54:04 +0530

    Add files via upload

---
+++ datatype1.py
@@ -0,0 +1,24 @@
+#text type :- string
+x = "shivani"
+print(type(x))
+
+#Numeric Type :- Integer,float,complex
+x1 = 20
+print(type(x1))
+
+x2 = 20.34
+print(type(x2))
+
+x3 = 4j
+print(type(x3))
+
+#Sequence Types :- list, tuple, range
+
+x4 = ["apple", "banana", "cherry"]
+print(type(x4))
+
+x5 = ("apple", "banana", "cherry")
+print(type(x5))
+
+x6 = range(6)
+print(type(x6))
